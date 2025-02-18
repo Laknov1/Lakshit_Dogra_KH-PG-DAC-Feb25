ca2437a26316c5bf355b4a1b7c41058047142f2b
Lakshit Dogra <lakshitdogra.jammu@gmail.com>
2025-02-18 16:20:17 +0530

    Add files via upload

---
+++ Assignment1_18feb/Q2.java
@@ -0,0 +1,22 @@
+//check negative number
+
+class Q2{
+	public static void main(String args[]){
+
+//enter the number
+
+	int n = -4;
+
+//if block with the condition if the number is greater than zero will print "not negative"
+
+	if(n>0){
+		System.out.println("Number is not Negative");
+	}
+
+//else block if the number is not greater than zero will print "negative"
+
+	else{
+		System.out.println("Number is negative");
+	}
+	}
+}
