653f66224ade09ad4221aff7f3acb3511c365696
Lakshit Dogra <lakshitdogra.jammu@gmail.com>
2025-02-23 21:05:21 +0530

    Add files via upload

---
+++ Week1_Logic-Building/Assignment1/Q5.java
@@ -0,0 +1,18 @@
+//print area of a square
+
+class Q5{
+	public static void main(String args[]){
+
+//enter the side of the square
+
+	int s = 4;
+
+//formula for area of square is side x side
+
+	int area = s*s;
+
+//print area as output
+
+	System.out.println(area);
+	}
+}
